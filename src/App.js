7c7f4808132baf243805ccde62ab2b02c2f3b189
syedkhalidashraf <i.khalidashraf@gmail.com>
2022-06-07 20:27:46 -0400

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import BoilingVerdict from "./components/boilingVerdict/boilingVerdict.component";
 import TemperatureInput from "./components/temperatureInput/temperatureInput.component";
 
+// Todo: Next make it so that when celsius is entered it should also show converted fahrenheit value and vice versa
+
 function App() {
 	const [temperature, setTemperature] = useState(0);
 	const [scaleType, setScaleType] = useState("c");
